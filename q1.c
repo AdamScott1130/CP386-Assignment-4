b06478b6cda04ccfac116b76f8f9694151395ebd
Adam Scott <101595410+AdamScott1130@users.noreply.github.com>
2022-04-04 22:53:05 -0400

    q1.c
    Fixed issue with safe_state and implemented status

--- q1.c
+++ q1.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+
+#define NUM_PROCESSES 5
+#define NUM_RESOURCES 4
+
+
 // Purpose: This program implements the Banker's Algorithm of resource allocation to
 // avoid deadlocks.
 
@@ -9,28 +14,32 @@
 // Function definitions
 
 
-int safeState(int threadID, int request[4], int allocated[5][4], int need[5][4], int available[4]) {
+int safeState(int threadID, int request[NUM_RESOURCES], int allocated[NUM_PROCESSES][NUM_RESOURCES], int need[NUM_PROCESSES][NUM_RESOURCES], int available[NUM_RESOURCES]) {
+
+	int testStatus = status(available, allocated, allocated, need);
+
 	// safeState checks whether the program would be in a safe state if the input request
 	// could leave the system in a deadlock.
 	// Input: array representing the resource request and the current maximum and available arrays.
-	// Output: 1 if the request results in a safe state, 0 if it may result in a deadlock
+	// Output: 0 if the request results in a safe state, -1 if it may result in a deadlock
 
 	// First, add the request to the thread's row in the need matrix
-	for (int i = 0; i < 4; i += 1) {
+	for (int i = 0; i < NUM_RESOURCES; i += 1) {
 		need[threadID][i] += request[i];
+		printf("%d\n", request[i]);
 	}
 
 	// Next, find a safe sequence (safety algorithm)
-	int finish[5] = {0,0,0,0,0};
-	int safeCounter = 5;
-	int sequence[5];
+	int finish[NUM_PROCESSES] = {0,0,0,0,0};
+	int safeCounter = NUM_PROCESSES;
+	int sequence[NUM_PROCESSES];
 	int flag = 0;
 	while (safeCounter != 0) {
 		// reset flag to 0 on each iteration. If at the end, flag = 0, then it did not
 		// reach 'flag = 1' on every iteration. That means that not every process was able to be allocated
 		// its resources and execute, so the state would be unsafe. If flag = 1 at the end, then we have a safe state.
 		flag = 0;
-		for(int i = 0; i < 5; i += 1) {
+		for(int i = 0; i < NUM_PROCESSES; i += 1) {
 			if(finish[i] == 0) { // check that the thread has not already been executed and had its resources freed.
 				int j;
 				for (j = 0; j < 5; j += 1) {
@@ -41,43 +50,43 @@ int safeState(int threadID, int request[4], int allocated[5][4], int need[5][4],
 				}
 				// If we reach the end with j = # of resources, then all resources have more available
 				// instances than process i has need, i.e., this process can execute
-				if (j == 5) {
+				if (j == NUM_PROCESSES) {
 					// mark that process i can run
 					finish[i] = 1;
 					// must reach this line on final iteration in order to be safe
 					flag = 1;
 					safeCounter += -1;
 					// give its resources back to the CPU
-					for (int k = 0; k < 5; k += 1) {
+					for (int k = 0; k < NUM_PROCESSES; k += 1) {
 						available[k] += allocated[i][k];
 					}
 					break;
 				}
 			}
 		}
-		// not a safe state, so return 0
+		// not a safe state, so return -1
 		if (flag == 0) {
-			return 0;
+			return -1;
 		}
 	}
-	// safe state, so return 1
-	return 1;
+	// safe state, so return 0
+	return 0;
 }
 
 
 
 
 
-int RQ(int threadID, int request[4], int allocated[5][4], int need[5][4], int available[4]){
+int RQ(int threadID, int request[NUM_RESOURCES], int allocated[NUM_PROCESSES][NUM_RESOURCES], int need[NUM_PROCESSES][NUM_RESOURCES], int available[NUM_PROCESSES][NUM_RESOURCES]){
 	// RQ is a function that requests resources based on the user's input.
 	// Input: array of integers representing a resource request and and the current maximum and available arrays.
-	// Output: 0 if the resources were successfully allocated, otherwise 1.
-	if (safeState(threadID, request, allocated, need, available)) {
+	// Output: 0 if the resources were successfully allocated, otherwise -1.
+	if (safeState(threadID, request, allocated, need, available) == 0) {
 		printf("Request results in safe state! Allocating resources...!\n");
 		return 0;
 	}
 	printf("Request results in unsafe state. Thread %d must wait.\n", threadID);
-	return 1;
+	return -1;
 }
 
 
@@ -93,8 +102,39 @@ int RL(){
 	return 1;
 }
 
-int status() {
+int status(int available[NUM_RESOURCES], int maximum[NUM_PROCESSES][NUM_RESOURCES], int allocated[NUM_PROCESSES][NUM_RESOURCES], int need[NUM_PROCESSES][NUM_RESOURCES]) {
 	// status prints the available, maximum, allocation, and need arrays.
+
+	printf("Available Resources: \n");
+	for (int i = 0; i < NUM_RESOURCES; i += 1) {
+		printf("%d ", available[i]);
+	}
+	printf("\n");
+
+	printf("Maximum: \n");
+	for (int i = 0; i < NUM_PROCESSES; i += 1) {
+		for (int j = 0; j < NUM_RESOURCES; j += 1) {
+			printf("%d ", maximum[i][j]);
+		}
+		printf("\n");
+	}
+
+	printf("Allocated: \n");
+	for (int i = 0; i < NUM_PROCESSES; i += 1) {
+		for (int j = 0; j < NUM_RESOURCES; j += 1) {
+			printf("%d ", allocated[i][j]);
+		}
+		printf("\n");
+	}
+
+	printf("Need: \n");
+	for (int i = 0; i < NUM_PROCESSES; i += 1) {
+		for (int j = 0; j < NUM_RESOURCES; j += 1) {
+			printf("%d ", need[i][j]);
+		}
+		printf("\n");
+	}
+
 	return 0;
 }
 
@@ -104,6 +144,8 @@ int run() {
 }
 int exitProgram() {
 	// terminates the program
+	printf("Exiting...\n");
+
 	exit(0);
 }
 
@@ -117,20 +159,18 @@ int main(int argc, char *argv[]){
 	}
 
 	printf("HERE1\n");
-	int numRows = 5; // number of threads
-	int numCols = 4; // number of resources
 
-	int maximum[5][4] = {{6, 4, 7, 3}, {4, 2, 3, 2}, {2, 5, 3, 3}, {6, 3, 3, 2}, {5, 5, 7, 5}};
-	int allocated[5][4] = {{0,0,0,0}, {0,0,0,0}, {0,0,0,0}, {0,0,0,0}, {0,0,0,0}};
-	int need[5][4] = {{0,0,0,0}, {0,0,0,0}, {0,0,0,0}, {0,0,0,0}, {0,0,0,0}};
+	int maximum[NUM_PROCESSES][NUM_RESOURCES] = {{6, 4, 7, 3}, {4, 2, 3, 2}, {2, 5, 3, 3}, {6, 3, 3, 2}, {5, 5, 7, 5}};
+	int allocated[NUM_PROCESSES][NUM_RESOURCES] = {{0,0,0,0}, {0,0,0,0}, {0,0,0,0}, {0,0,0,0}, {0,0,0,0}};
+	int need[NUM_PROCESSES][NUM_RESOURCES] = {{0,0,0,0}, {0,0,0,0}, {0,0,0,0}, {0,0,0,0}, {0,0,0,0}};
 	printf("HERE2\n");
-	int available[argc-1];
-	for (int i = 1; i < argc-1; i+=1) {
+	int available[NUM_RESOURCES];
+	for (int i = 1; i < argc; i+=1) {
 		available[i-1] = atoi(argv[i]);
 	}
 
-	for (int i = 0; i < numRows; i += 1) {
-		for (int j = 0; j < numCols; j += 1)  {
+	for (int i = 0; i < NUM_PROCESSES; i += 1) {
+		for (int j = 0; j < NUM_RESOURCES; j += 1)  {
 			need[i][j] = maximum[i][j] - allocated[i][j];
 			// printf(" %d ", need[i][j]);
 		}
